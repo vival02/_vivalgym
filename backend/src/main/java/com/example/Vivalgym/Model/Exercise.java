b5123868d18c4e3203fa7d6adf6c0a1c2d9cae1b
vival02 <camilla.vivaldi02@gmail.com>
2023-12-06 12:05:59 +0100

    Update Exercise.java

--- backend/src/main/java/com/example/Vivalgym/Model/Exercise.java
+++ backend/src/main/java/com/example/Vivalgym/Model/Exercise.java
@@ -12,13 +12,16 @@ public class Exercise {
     @SequenceGenerator(name = "exercise_gen",sequenceName = "exercise_seq",initialValue =1, allocationSize = 1)
     private int idExercise;
     private String nome;
+    private String nomeIng;
     private String tipologia;
+    private String rating;
     private String gruppoMuscolare;
-    private String Img ;
+    private String immagine1 ;
+    private String immagine2 ;
     private String descrizione;
     private String Attrezzatura;
     private String livello;
-
+    private String linkApprofondimento;
     public void setIdExercise(int idExercise) {
         this.idExercise = idExercise;
     }
@@ -29,6 +32,46 @@ public class Exercise {
 
     public void setAttrezzatura(String attrezzatura) {
         Attrezzatura = attrezzatura;
+    }
+
+    public String getNomeIng() {
+        return nomeIng;
+    }
+
+    public void setNomeIng(String nomeIng) {
+        this.nomeIng = nomeIng;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getImmagine1() {
+        return immagine1;
+    }
+
+    public void setImmagine1(String immagine1) {
+        this.immagine1 = immagine1;
+    }
+
+    public String getImmagine2() {
+        return immagine2;
+    }
+
+    public void setImmagine2(String immagine2) {
+        this.immagine2 = immagine2;
+    }
+
+    public String getLinkApprofondimento() {
+        return linkApprofondimento;
+    }
+
+    public void setLinkApprofondimento(String linkApprofondimento) {
+        this.linkApprofondimento = linkApprofondimento;
     }
 
     //
